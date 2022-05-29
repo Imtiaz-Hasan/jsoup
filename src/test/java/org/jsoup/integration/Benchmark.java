f67bc2ec413209d8132d9168855b5a92472a7291
Md Imtiaz Hasan <imtiaz.hasan121@gmail.com>
2022-05-29 22:20:04 +0600

    Initial commit

---
+++ src/test/java/org/jsoup/integration/Benchmark.java
@@ -0,0 +1,33 @@
+package org.jsoup.integration;
+
+import java.util.Date;
+
+public class Benchmark {
+    public static void run(Runnable a, Runnable b, int count) {
+        long aMillis;
+        long bMillis;
+
+        print("Running test A (x%d)", count);
+        aMillis = time(a, count);
+        print("Running test B");
+        bMillis = time(b, count);
+
+        print("\nResults:");
+        print("A: %.2fs", aMillis / 1000f);
+        print("B: %.2fs", bMillis / 1000f);
+        print("\nB ran in %.2f %% time of A\n", (bMillis *1f / aMillis * 1f) * 100f);
+    }
+
+    private static long time(Runnable test, int count) {
+        Date start = new Date();
+        for (int i = 0; i < count; i++) {
+            test.run();
+        }
+        Date end = new Date();
+        return end.getTime() - start.getTime();
+    }
+
+    private static void print(String msgFormat, Object... msgParams) {
+        System.out.println(String.format(msgFormat, msgParams));
+    }
+}
